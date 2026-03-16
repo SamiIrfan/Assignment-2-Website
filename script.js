6fe1b0d4f15789ffb4863b3d1cff4589cb27ae98
SamiIrfan <samizerosix@gmail.com>
2026-03-16 09:56:02 +0000

    Added 3 Last Pages and Updated JS

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const burger = document.getElementById("burger");
 const navLinks = document.querySelector(".nav-links");
 
 burger.addEventListener("click", () => {
-
 navLinks.classList.toggle("show");
+});
+
+
+/* Navigation Highlight */
+
+const links = document.querySelectorAll(".nav-links a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+
+const linkPage = link.getAttribute("href");
+
+if(linkPage === currentPage){
+link.classList.add("active");
+}
 
 });
